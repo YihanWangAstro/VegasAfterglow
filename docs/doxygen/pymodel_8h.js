39fb3ca95b23ea6ee37031ae8b0439c2a8daf087
YihanWangAstro <YihanWangAstro@users.noreply.github.com>
2025-05-14 11:54:00 +0000

    deploy: d4975393c5f1c3dd54a6f70367943478ce4c8586

--- docs/doxygen/pymodel_8h.js
+++ docs/doxygen/pymodel_8h.js
@@ -4,9 +4,9 @@ var pymodel_8h =
     [ "PyObserver", "classPyObserver.html", "classPyObserver" ],
     [ "PyRadiation", "classPyRadiation.html", "classPyRadiation" ],
     [ "PyModel", "classPyModel.html", "classPyModel" ],
-    [ "PyGaussianJet", "pymodel_8h.html#a73496c9fa085b5258d74ec9023a09000", null ],
+    [ "PyGaussianJet", "pymodel_8h.html#af1674b6df8d031485f34cab44c50fa0e", null ],
     [ "PyISM", "pymodel_8h.html#a43af01d16838e6513792deaeb727fbe5", null ],
-    [ "PyPowerLawJet", "pymodel_8h.html#a293b86692928c43e2623e5898be88396", null ],
-    [ "PyTophatJet", "pymodel_8h.html#a0f20860f4dd5636fb242114adbe961bd", null ],
+    [ "PyPowerLawJet", "pymodel_8h.html#a54e2dccadd522db2228e9f0b9bddc97e", null ],
+    [ "PyTophatJet", "pymodel_8h.html#a7de598d3b113b25bbd1ecbbe9afb126d", null ],
     [ "PyWind", "pymodel_8h.html#af5469e939faefb859e1f4b22e1e0bd02", null ]
 ];
