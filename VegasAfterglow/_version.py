5f7e18026011c7e3b1c7eec117fc66fb404787e8
Yihan Wang <yihan.astro@gmail.com>
2026-03-01 11:29:19 -0600

    simplify pre-commit-hook

--- VegasAfterglow/_version.py
+++ VegasAfterglow/_version.py
@@ -27,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "1.1.1.dev162+gd6d76bad9.d20260301"
-__version_tuple__ = version_tuple = (1, 1, 1, "dev162", "gd6d76bad9.d20260301")
+__version__ = version = "1.1.1.dev163+gb010c8eda.d20260301"
+__version_tuple__ = version_tuple = (1, 1, 1, "dev163", "gb010c8eda.d20260301")
 
-__commit_id__ = commit_id = "gd6d76bad9"
+__commit_id__ = commit_id = "gb010c8eda"
